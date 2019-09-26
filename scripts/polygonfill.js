6ef5d46d0e7207375886a0e2f5797b0fdd630821
Sosa Edison <sosa_edison@hotmail.com>
2019-09-26 08:09:38 -0500

    attempting to draw lines now

--- scripts/polygonfill.js
+++ scripts/polygonfill.js
@@ -76,6 +76,7 @@ function DrawPolygon(polygon) {
     var polygon_array = polygon.vertices;
     var vertex_1 = {};
     var vertex_2 = {};
+    var polygon_y_max = 0;
     for(k =0; k < polygon_array.length; k++) {
         vertex_1 = {
             x: polygon_array[k].x,
@@ -95,25 +96,30 @@ function DrawPolygon(polygon) {
         var y_max = Math.max(vertex_1.y, vertex_2.y);
         var y_min = Math.min(vertex_1.y, vertex_2.y);
         var x_min = Math.min(vertex_1.x, vertex_2.x);
+        if(polygon_y_max < y_max) {
+            polygon_y_max = y_max;
+        }
         /**
          * Find x-min and y-min points
          * CHECK FOR HORIZONTAL LINES AND OMIT FROM ET
          */
 
-
         var delta_y = vertex_2.y - vertex_1.y;
         var delta_x = vertex_2.x - vertex_1.x;
 
-        edge_table[y_min].InsertEdge(new EdgeEntry(y_max, x_min, delta_x, delta_y));
+        var entry = new EdgeEntry(y_max, x_min, delta_x, delta_y);
+        
+        edge_table[y_min].InsertEdge(entry);
         //console.log(edge_table[y_min]);
         
     }
+    //console.log(edge_table)
 
     // Step 2: set y to first scan line with an entry in ET
     var i;
     var lowest_y = polygon_array[0].y;
     for(i=0; i < polygon_array.length; i++) {
-        temp_y = polygon_array[i].y;
+        var temp_y = polygon_array[i].y;
         if(temp_y < lowest_y) {
             lowest_y = temp_y;
         }
@@ -130,33 +136,57 @@ function DrawPolygon(polygon) {
     //   f) Update x-values for all remaining entries in the AL (increment by 1/m)
 
     //add from the edgetable lowest entry to the active list 
-   
-    while((edge_table[y] != null) && (active_list != null)) {
-
-        if(edge_table[y].first_entry != null) {
-            active_list[y] = edge_table[y];
-        } else {
-            var prev_edge = active_list[y-1];
-
-            console.log(prev_edge)
-           
-            
-            // while(prev_edge.first_entry != null) {
-            //     var slope = prev_edge.first_entry.inv_slope;
-            //     var new_x = prev_edge.first_entry.x + slope;
-            //     var new_y = prev_edge.first_entry.y_max;
-
-            //     new_list = new EdgeList(new EdgeEntry(new_y, new_x, slope));
-            //     active_list[y] = new_list;
-
-            //     prev_edge = prev_edge.first_entry;
-            // }
+    // we have to also add the references to each edgeEntry
+    var i;
+    var cur_entry;
+    var linked_entry;
+    for(i = y; i < polygon_y_max; i++) {
+        cur_entry = edge_table[i].first_entry
+        while(cur_entry !== null) {
+            active_list.InsertEdge(cur_entry);
+            cur_entry = cur_entry.next_entry;
         }
+    }
+    active_list.SortList()
+    /** 
+    var active_entry = active_list.first_entry;
+    var i;
+    while(active_entry !== null) {
+
+        for(i = y; i < polygon_y_max; i++) {
+            if(i === active_entry.y_max) {
+                active_list.RemoveCompleteEdges(i);
+            }
+        }
+        active_entry = active_entry.next_entry;
+    } 
+    **/
+
+    var point;
+    var p1, p2;
+    point = active_list.first_entry;
+    
+    while(y !== polygon_y_max) {
+        p1 = {
+            x: point.x + 1/point.inv_slope,
+            y: point.y + 1/point.inv_slope
+        }
+        p2 = {
+            x: point.next_entry.x + 1/point.next_entry.inv_slope,
+            y:point.next_entry.y + 1/point.next_entry.inv_slope
+        }
+        DrawLine(p1.x, p1.y, p2.x, p2.y);
+        console.log(p1)
+        console.log(p2)
+        if(y === point.y_max) {
+            active_list.RemoveCompleteEdges(y);  
+        } else {
+            
+        }
+
         y++;
     }
-
     console.log(active_list);
-    
 }
 
 // SelectNewPolygon(): triggered when new selection in drop down menu is made
