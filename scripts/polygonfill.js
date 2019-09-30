7bed914b33de34bf67fc0d4affecc8618cc5d352
Sosa Edison <sosa_edison@hotmail.com>
2019-09-29 23:07:24 -0500

    Final touches added vertices

--- scripts/polygonfill.js
+++ scripts/polygonfill.js
@@ -4,37 +4,46 @@ var polygons = {
     convex: {
         color: 'red', // choose color here!
         vertices: [
-            // {x: 100, y:100},
-            // {x: 110, y:300},
-            // {x: 300, y:500},
-            // {x: 600, y:300},
-            // {x: 300, y: 50},
-            // {x: 600, y:100}
-
-            {x:100, y:100},
-            {x:600, y:150},
-            {x:700, y:400},
-            {x:500, y:450},
-            {x:200, y:350}
             // fill in vertices here!
+            {x:100, y:100},
+            {x:400, y:150},
+            {x:500, y:500},
+            {x:200, y:350},
+            
         ]
     },
     concave: {
-        color: '', // choose color here!
+        color: 'blue', // choose color here!
         vertices: [
             // fill in vertices here!
+            {x:100,y:100},
+            {x:200,y:200},
+            {x:150,y:300},
+            {x:400,y:350},
+            {x:350,y:75}
+            
+            
         ]
     },
     self_intersect: {
-        color: '', // choose color here!
+        color: 'black', // choose color here!
         vertices: [
             // fill in vertices here!
+            {x:100,y:100},
+            {x:500,y:500},
+            {x:450,y:300},
+            {x:150,y:450}
         ]
     },
     interior_hole: {
-        color: '', // choose color here!
+        color: 'green', // choose color here!
         vertices: [
             // fill in vertices here!
+            {x:100,y:100},
+            {x:300,y:600},
+            {x:500,y:50},
+            {x:50,y:350},
+            {x:400,y:400}
         ]
     }
 };
@@ -99,21 +108,15 @@ function DrawPolygon(polygon) {
         if(polygon_y_max < y_max) {
             polygon_y_max = y_max;
         }
-        /**
-         * Find x-min and y-min points
-         * CHECK FOR HORIZONTAL LINES AND OMIT FROM ET
-         */
-
         var delta_y = vertex_2.y - vertex_1.y;
         var delta_x = vertex_2.x - vertex_1.x;
 
         var entry = new EdgeEntry(y_max, x_min, delta_x, delta_y);
         
         edge_table[y_min].InsertEdge(entry);
-        //console.log(edge_table[y_min]);
+        console.log(edge_table[y_min]);
         
     }
-    //console.log(edge_table)
 
     // Step 2: set y to first scan line with an entry in ET
     var i;
@@ -125,8 +128,7 @@ function DrawPolygon(polygon) {
         }
     }
     var y = lowest_y;
-   
-
+    
     // Step 3: Repeat until ET[y] is NULL and AL is NULL
     //   a) Move all entries at ET[y] into AL
     //   b) Sort AL to maintain ascending x-value order
@@ -135,58 +137,26 @@ function DrawPolygon(polygon) {
     //   e) Increment y by 1
     //   f) Update x-values for all remaining entries in the AL (increment by 1/m)
 
-    //add from the edgetable lowest entry to the active list 
-    // we have to also add the references to each edgeEntry
     var i;
-    var cur_entry;
-    var linked_entry;
     for(i = y; i < polygon_y_max; i++) {
-        cur_entry = edge_table[i].first_entry
-        while(cur_entry !== null) {
-            active_list.InsertEdge(cur_entry);
-            cur_entry = cur_entry.next_entry;
-        }
-    }
-    active_list.SortList()
-    /** 
-    var active_entry = active_list.first_entry;
-    var i;
-    while(active_entry !== null) {
-
-        for(i = y; i < polygon_y_max; i++) {
-            if(i === active_entry.y_max) {
-                active_list.RemoveCompleteEdges(i);
+        var temp_entry = edge_table[i].first_entry;
+            while(temp_entry !== null){
+                active_list.InsertEdge(temp_entry);
+                temp_entry = temp_entry.next_entry;
             }
+        active_list.SortList();
+        active_list.RemoveCompleteEdges(i); 
+        temp_entry = active_list.first_entry;
+        while(temp_entry !== null) {
+            DrawLine(temp_entry.x, i, temp_entry.next_entry.x, i);
+            temp_entry = temp_entry.next_entry.next_entry;
         }
-        active_entry = active_entry.next_entry;
-    } 
-    **/
-
-    var point;
-    var p1, p2;
-    point = active_list.first_entry;
-    
-    while(y !== polygon_y_max) {
-        p1 = {
-            x: point.x + 1/point.inv_slope,
-            y: point.y + 1/point.inv_slope
+        temp_entry = active_list.first_entry;
+        while(temp_entry !== null) {
+            temp_entry.x = temp_entry.x + temp_entry.inv_slope;
+            temp_entry = temp_entry.next_entry;
         }
-        p2 = {
-            x: point.next_entry.x + 1/point.next_entry.inv_slope,
-            y:point.next_entry.y + 1/point.next_entry.inv_slope
-        }
-        DrawLine(p1.x, p1.y, p2.x, p2.y);
-        console.log(p1)
-        console.log(p2)
-        if(y === point.y_max) {
-            active_list.RemoveCompleteEdges(y);  
-        } else {
-            
-        }
-
-        y++;
     }
-    console.log(active_list);
 }
 
 // SelectNewPolygon(): triggered when new selection in drop down menu is made
